ad1d17ef95b0e056d8026f9e34f79a8c10e42d4b
SriMoorthy <srimoorthy27@gmail.com>
2024-08-22 16:59:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ class Person{
 		this.name = name;
 		this.age = age;
 	}
-	greet = () => console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
+	greet(){
+		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
+	}
 }
 
 class Employee extends Person {
@@ -12,7 +14,10 @@ class Employee extends Person {
 		super(name,age);
 		this.jobTitle = jobTitle;
 	}
-	jobGreet = () => console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
+	
+	jobGreet(){ 
+		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
+	}
 }
 
 // Do not change code below this line
